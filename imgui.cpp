408d26b8bebddd60d7939da0cfab25ae95aaf5c2
SShikhali <sshikhaliev@luxoft.com>
2024-05-05 17:44:53 +0200

    Eagle: added `Begin` and `End` hooks to be used by node editor

--- imgui.cpp
+++ imgui.cpp
@@ -6767,6 +6767,8 @@ bool ImGui::Begin(const char* name, bool* p_open, ImGuiWindowFlags flags)
     IM_ASSERT(g.WithinFrameScope);                  // Forgot to call ImGui::NewFrame()
     IM_ASSERT(g.FrameCountEnded != g.FrameCount);   // Called ImGui::Render() or ImGui::EndFrame() and haven't called ImGui::NewFrame() again yet
 
+    CallContextHooks(&g, ImGuiContextHookType_BeginWindow);
+
     // Find or create
     ImGuiWindow* window = FindWindowByName(name);
     const bool window_just_created = (window == NULL);
@@ -7692,6 +7694,8 @@ void ImGui::End()
     SetCurrentWindow(g.CurrentWindowStack.Size == 0 ? NULL : g.CurrentWindowStack.back().Window);
     if (g.CurrentWindow)
         SetCurrentViewport(g.CurrentWindow, g.CurrentWindow->Viewport);
+
+    CallContextHooks(&g, ImGuiContextHookType_EndWindow);
 }
 
 void ImGui::BringWindowToFocusFront(ImGuiWindow* window)
