1da6e176a183deef8af569d382c1927a7be4f19e
SShikhali <sshikhaliev@luxoft.com>
2025-07-09 00:35:54 +0200

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -29,10 +29,10 @@ project "ImGui"
     filter { "system:windows", "configurations:Debug" }
         runtime "Debug"
 
-    filter { "system:windows", "configurations:Release" }
+    filter { "system:windows", "configurations:ReleaseWithDebug" }
         runtime "Release"
         optimize "Speed"
 
-    filter { "system:windows", "configurations:Dist" }
+    filter { "system:windows", "configurations:Release" }
         runtime "Release"
         optimize "Speed"
